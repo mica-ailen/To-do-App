4e88fad8cc66bd75743221762c3839de4916200a
mica-ailen <101481046+mica-ailen@users.noreply.github.com>
2023-01-09 11:35:38 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './components/TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-app">
+     <TodoList/>
+    </div>
+  );
+}
+
+export default App;
