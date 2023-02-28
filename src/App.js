3bfeb878fa4f6d7e50154c7e517bd84d43c97e2e
mica-ailen <101481046+mica-ailen@users.noreply.github.com>
2023-02-28 12:02:41 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useState } from "react";
-import TodoList from "./TodoList";
+import './App.css';
+import TodoList from './components/TodoList';
+import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState({
-    //this is an object and they are the properties
-    id: 1,
-    name: "Todo 1",
-    complete: false,
-  });
   return (
-    <>
-      <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
-      <button>Clear Completed Todos</button>
-      <div>0 left to do</div>
-    </>
+    <div className="todo-app">
+     <TodoList/>
+    </div>
   );
 }
 
